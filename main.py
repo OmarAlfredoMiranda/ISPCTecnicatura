d72a396a13b12b71afe844a8586ab96e5f886fc6
OmarMiranda <OmarAlfredoMiranda@gmail.com>
2022-05-11 21:56:31 -0300

    prueba2

--- main.py
+++ main.py
@@ -2,10 +2,23 @@
 #Fecha: 08/05/2022
 #Descripcion: Prueba GitHub
 
+1
+
 def suma(a,b):
     return (a+b)
+
+def resta(a,b):
+    return (a-b)
+
+def prod(a,b):
+    return(a*b)    
 
 #if __name__ == "_main__*":
 
 print ("Primer Programa en Python")
+print()
 print ("la suma es: ",str(suma(2,3)) )
+print()
+print ("la resta es: ",str(resta(2,3)) )
+print()
+print("la prod es: ",str(prod(2,3)))
